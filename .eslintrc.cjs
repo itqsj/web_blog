0b8ab61859bb0c17c7bda89d8491a63763321db9
qinshijie <qinshijie@qq.com>
2022-10-15 14:30:04 +0800

    feat: 新增eslint+prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+// 修改eslint配置与添加规则
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // 新增
+  ],
+  overrides: [],
+  // 新增，解析vue文件 开始
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  // 新增，解析vue文件 结束
+  plugins: ['vue', '@typescript-eslint'],
+  // 添加规则
+  rules: {
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        extendDefaults: true,
+        types: {
+          '{}': false,
+        },
+      },
+    ],
+  },
+};
